40387dd2b952b9f819c81c159aa93448a5092eb6
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-09-13 15:29:26 -0700

    AngularServicesP2Checkpoint_beforePauseMethod

--- scripts/services/Fixtures.js
+++ scripts/services/Fixtures.js
@@ -74,7 +74,7 @@
         Fixtures.getCollection = function (numberOfAlbums) {
             this.albums=[];
             for (var i=0; i<numberOfAlbums; i++){
-                this.albums.push(angular.copy(albumPicasso));
+                this.albums.push(Fixtures.getAlbum());
             }
             return this.albums;
         };
