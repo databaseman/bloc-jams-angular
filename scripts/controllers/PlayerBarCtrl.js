d44aef86812c4bf613f573ab6e881ff3eb235039
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-09-14 15:52:31 -0700

    servicesP3Checkpoint_beforePreNext

---
+++ scripts/controllers/PlayerBarCtrl.js
@@ -0,0 +1,11 @@
+ (function() {
+     function PlayerBarCtrl(Fixtures, SongPlayer) {
+         this.albumData = Fixtures.getAlbum();
+         this.songPlayer = SongPlayer;
+         console.log( this.songPlayer);
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('PlayerBarCtrl', ['Fixtures', 'SongPlayer', PlayerBarCtrl]);
+ })();
--- scripts/services/SongPlayer.js
+++ scripts/services/SongPlayer.js
@@ -5,7 +5,7 @@
          * @desc Buzz object audio file
          * @type {Object}
          */
-        var currentSong = null;
+        SongPlayer.currentSong = null;
         var currentBuzzObject = null;
 
         /**
@@ -16,7 +16,7 @@
         var setSong = function (song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -24,7 +24,7 @@
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
 
         /**
@@ -32,24 +32,33 @@
          * @desc Play currently playing song
          * @param {Object} song
          */
-        var playSong = function( song ){
+        var playSong = function (song) {
             currentBuzzObject.play();
             song.playing = true;
         };
-        
-        
+        /**
+         * @function play
+         * @desc Play current or new song
+         * @param {Object} song
+         */
         SongPlayer.play = function (song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     playSong(song);
                 }
             }
         };
-
+        /**
+         * @function pause
+         * @desc Pause current song
+         * @param {Object} song
+         */
         SongPlayer.pause = function (song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         };
